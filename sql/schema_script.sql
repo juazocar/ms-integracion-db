1f5a6c06ad1cd343e2fd5767a589ad3abe1ca219
Juan Azocar <juazocar@profesor.duoc.cl>
2023-05-11 19:21:49 -0400

    sad

--- sql/schema_script.sql
+++ sql/schema_script.sql
@@ -86,6 +86,7 @@ CREATE TABLE `persona` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
 --
 -- Dumping data for table `persona`
 --
